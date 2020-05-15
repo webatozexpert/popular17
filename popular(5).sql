bc70a14e9ab44459ca61ed9eefce38f0d63fa6e1
Lutforbd84 <webatozexpert@gmail.com>
2020-05-15 20:18:27 +0600

    commit

--- popular(5).sql
+++ popular(5).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2020 at 06:28 PM
+-- Generation Time: May 15, 2020 at 04:16 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.1
 
@@ -57,7 +57,7 @@ CREATE TABLE `logos` (
 --
 
 INSERT INTO `logos` (`id`, `image`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(6, '202005131425logo.png.png', 3, 3, '2020-05-13 08:25:24', '2020-05-13 08:25:42');
+(6, '202005150812logo.png', 3, 6, '2020-05-13 08:25:24', '2020-05-15 02:12:17');
 
 -- --------------------------------------------------------
 
@@ -80,7 +80,58 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (6, '2014_10_12_100000_create_password_resets_table', 1),
 (7, '2019_08_19_000000_create_failed_jobs_table', 1),
 (8, '2020_05_13_114631_create_logos_table', 2),
-(9, '2020_05_14_151539_create_sliders_table', 3);
+(9, '2020_05_14_151539_create_sliders_table', 3),
+(10, '2020_05_15_034434_create_missions_table', 4),
+(11, '2020_05_15_055216_create_news_events_table', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `missions`
+--
+
+CREATE TABLE `missions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `missions`
+--
+
+INSERT INTO `missions` (`id`, `image`, `title`, `description`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(3, '202005150813mission.jpg', 'This is my Love Project Mission', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', 6, 6, '2020-05-14 23:20:29', '2020-05-15 02:13:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `news_events`
+--
+
+CREATE TABLE `news_events` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `date` date DEFAULT NULL,
+  `image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `short_title` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `long_title` longtext COLLATE utf8mb4_unicode_ci,
+  `created_by` int(11) DEFAULT NULL,
+  `updated_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `news_events`
+--
+
+INSERT INTO `news_events` (`id`, `date`, `image`, `short_title`, `long_title`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
+(1, '2020-05-07', '202005151014news1.jpg', 'First Slide', '\"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum\"\"\"\"\"', 6, 3, '2020-05-15 01:02:07', '2020-05-15 04:14:03');
 
 -- --------------------------------------------------------
 
@@ -116,9 +167,10 @@ CREATE TABLE `sliders` (
 --
 
 INSERT INTO `sliders` (`id`, `image`, `short_title`, `long_title`, `created_by`, `updated_by`, `created_at`, `updated_at`) VALUES
-(1, '202005141621slider4.jpeg', 'First Slide', 'This is a description for the first slide.', 3, 3, '2020-05-14 10:08:14', '2020-05-14 10:21:59'),
-(2, '202005141608slider5.jpg', 'First Slide', 'This is a description for the first slide.', 3, NULL, '2020-05-14 10:08:34', '2020-05-14 10:08:34'),
-(4, '202005141627slider6.jpg', 'First Slide', 'This is a description for the first slide.', 3, NULL, '2020-05-14 10:27:05', '2020-05-14 10:27:05');
+(1, '202005151327slider4.jpeg', 'First Slide', 'This is a description for the first slide.', 3, 3, '2020-05-14 10:08:14', '2020-05-15 07:27:38'),
+(2, '202005151327slider5.jpg', 'First Slide', 'This is a description for the first slide.', 3, 3, '2020-05-14 10:08:34', '2020-05-15 07:27:47'),
+(4, '202005151327slider6.jpg', 'First Slide', 'This is a description for the first slide.', 3, 3, '2020-05-14 10:27:05', '2020-05-15 07:27:58'),
+(5, '202005151347news1.jpg', 'First Slide', 'This is a description for the first slide.', 3, NULL, '2020-05-15 07:47:26', '2020-05-15 07:47:26');
 
 -- --------------------------------------------------------
 
@@ -148,8 +200,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `usertype`, `name`, `email`, `email_verified_at`, `password`, `mobile`, `address`, `gender`, `image`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'Admin', 'Rojatul Mariya Saoda', 'saoda2020@gmail.com', NULL, '$2y$10$5wnfNa.tu1Thg4uRbKOGSeg7F/HU11DFCa1iZOCmmUapFqhyagLwS', '+8801958499092', 'Khidropery', 'Female', '202005131322IMG_20140101_144606.jpg', 1, NULL, '2020-05-08 08:17:24', '2020-05-13 07:22:33'),
-(6, 'Admin', 'Md Solaiman Rahman', 'solaiman@gmail.com', NULL, '$2y$10$7E5GlanqdcwfexEe51oiTO2lZrpUwErjWjEwfTyeLoBcb14djjqRq', '+8801716745845', 'Bugura', 'male', '202005130628DSC_0023.jpg', 1, NULL, '2020-05-10 07:33:14', '2020-05-13 00:28:25'),
+(3, 'Admin', 'Rojatul Mariya Saoda', 'saoda2020@gmail.com', NULL, '$2y$10$5wnfNa.tu1Thg4uRbKOGSeg7F/HU11DFCa1iZOCmmUapFqhyagLwS', '+8801958499092', 'Khidropery', 'Female', '202005150944news3.jpg', 1, NULL, '2020-05-08 08:17:24', '2020-05-15 03:44:15'),
+(6, 'Admin', 'Md Solaiman Rahman', 'solaiman@gmail.com', NULL, '$2y$10$7E5GlanqdcwfexEe51oiTO2lZrpUwErjWjEwfTyeLoBcb14djjqRq', '+8801716745845', 'Bugura', 'male', '202005150816scrl.jpg', 1, NULL, '2020-05-10 07:33:14', '2020-05-15 02:16:41'),
 (7, 'User', 'Mst Moriom Siddika', 'mou@gmail.com', NULL, '$2y$10$6KCJPrDwNCt7CAh6SwJE1eXgxsUycTHD0aaKpJXuHZB.SCnV0lPTK', '+8801716745845', 'Bugura', 'Female', '202005130630IMG_20140103_200008.JPG', 1, NULL, '2020-05-10 22:34:51', '2020-05-13 00:30:05');
 
 --
@@ -172,6 +224,18 @@ ALTER TABLE `logos`
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `missions`
+--
+ALTER TABLE `missions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `news_events`
+--
+ALTER TABLE `news_events`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -207,19 +271,31 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `logos`
 --
 ALTER TABLE `logos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `missions`
+--
+ALTER TABLE `missions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `news_events`
+--
+ALTER TABLE `news_events`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sliders`
 --
 ALTER TABLE `sliders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
